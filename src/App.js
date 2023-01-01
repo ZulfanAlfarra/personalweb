86a48c44cebd4011601e453d659f9a79047df055
ZulfanAlfarra <zulfansyahidanalfarra.91@gmail.com>
2023-01-01 21:24:05 +0700

    update path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/personalweb/' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/project' element={<Project />} />
           <Route path='/project/:id' element={<DetailProject />} />
           <Route path='/experience' element={<Experience />} />
